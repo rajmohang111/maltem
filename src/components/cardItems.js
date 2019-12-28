715333d8edcd8938403e21948c1e59bf4787637b
unknown <rajimohan.gopalan@logicline.de>
2019-12-28 21:08:33 +0800

    folder structure changed

---
+++ src/components/cardItems.js
@@ -0,0 +1,133 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+<style>
+.list-items {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-content: start;
+  padding: 0 0.6rem 0.5rem;
+  overflow-y: auto;
+  height : 200px;
+}
+.list-items::-webkit-scrollbar {
+  width: 1.6rem;
+}
+.list-items::-webkit-scrollbar-thumb {
+  background-color: #c4c9cc;
+  border-right: 0.6rem solid #e2e4e6;
+}
+.list-items li {
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 1.3;
+  background-color: #fff;
+  padding: 0.65rem 0.6rem;
+  color: #4d4d4d;
+  border-bottom: 0.1rem solid #ccc;
+  border-radius: 0.3rem;
+  margin-bottom: 0.6rem;
+  word-wrap: break-word;
+  cursor: pointer;
+}
+.list-items li:last-of-type {
+  margin-bottom: 0;
+}
+.list-items li:hover {
+  background-color: #eee;
+}
+  </style>
+  <ul class="list-items">
+</ul>`;
+
+class CardItems extends HTMLElement {
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+
+    this.cards = [
+      {
+        "id": 1,
+        "title": "Card 1",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "columnId": 1
+      },
+      {
+        "id": 2,
+        "title": "Card 2",
+        "description": "Quisque et pellentesque sem.",
+        "columnId": 1
+      },
+      {
+        "id": 3,
+        "title": "Card 3",
+        "description": "Nulla porttitor erat a sollicitudin volutpat.",
+        "columnId": 1
+      },
+      {
+        "id": 4,
+        "title": "Card 4",
+        "description": "Quisque id scelerisque felis, sit amet scelerisque nunc.",
+        "columnId": 2
+      },
+      {
+        "id": 5,
+        "title": "Card 5",
+        "description": "Suspendisse posuere ipsum at dui lacinia, ut faucibus lectus mollis.",
+        "columnId": 2
+      }
+    ];
+
+    for(let i=0;i<this.cards.length;i++) {
+      template.content.querySelector('.list-items').innerHTML += `
+      <li>${this.cards[i].title}</li>    
+      `;
+    }
+
+
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.$headline = this._shadowRoot.querySelector('h2');
+    this.$span = this._shadowRoot.querySelector('span');
+  }
+
+  connectedCallback() {
+    if (!this.hasAttribute('color')) {
+      this.setAttribute('color', 'orange');
+    }
+
+    if (!this.hasAttribute('text')) {
+      this.setAttribute('text', '');
+    }
+
+
+
+    this._render();
+  }
+
+  static get observedAttributes() {
+    return ['color', 'text'];
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    switch (name) {
+      case 'color':
+        this._color = newVal;
+        break;
+      case 'text':
+        this._text = newVal;
+        break;
+    };
+
+    this._render();
+  }
+
+  _render() {
+   // this.$headline.style.color = this._color;
+  //  this.$span.innerHTML = this._text;
+  }
+}
+
+window.customElements.define('card-items', CardItems);
--- src/index.js
+++ src/index.js
@@ -1,42 +1,79 @@
-import './components/say-something.js';
+import './components/cardItems.js';
 
 const template = document.createElement('template');
 
 template.innerHTML = `
-  <style>
-    :host {
-      font-family: sans-serif;
-    }
-  </style>
+<style>
+.list {
+  flex: 0 0 27rem;
+  display: flex;
+  flex-direction: column;
+  background-color: #e2e4e6;
+  max-height: calc(100vh - 11.8rem);
+  border-radius: 0.3rem;
+  margin-right: 1rem;
+  margin: 20px;
+}
 
-  <div>
-    <h1>Web Components with Webpack Starter Kit</h1>
+.list:last-of-type {
+  margin-right: 0;
+}
 
-    Text: <input type="text" />
-
-    <say-something></say-something>
-    <say-something color="red"></say-something>
-  </div>
+.list-title {
+  font-size: 1.4rem;
+  font-weight: 700;
+  color: #333;
+  padding: 1rem;
+}
+</style>
+<section class="lists-container">
+  </section>
 `;
 
-class App extends HTMLElement {
+
+
+class Card extends HTMLElement {
   constructor() {
     super();
 
     this._shadowRoot = this.attachShadow({ mode: 'open' });
+
+    console.log(' template.content',  template.content.querySelector('.lists-container').innerHTML);
+
+     this.columns =  [
+      {
+        "id": 1,
+        "title": "Column 1"
+      },
+      {
+        "id": 2,
+        "title": "Column 2"
+      }
+    ];
+
+    for(let i=0;i<this.columns.length;i++) {
+      template.content.querySelector('.lists-container').innerHTML += ` <div class="list">
+    
+      <h3 class="list-title">${this.columns[i].title}</h3>
+    
+      <card-items></card-items>
+    
+      </div>`;
+    }
+
     this._shadowRoot.appendChild(template.content.cloneNode(true));
 
     this.$input = this._shadowRoot.querySelector('input');
     this.$input.addEventListener('input', this._handleChange.bind(this));
 
-    this.$allSaySomething = this._shadowRoot.querySelectorAll('say-something');
+   // this.$allSaySomething = this._shadowRoot.querySelectorAll('say-something');
   }
 
   _handleChange() {
-    this.$allSaySomething.forEach(element => {
-      element.setAttribute('text', this.$input.value)
-    });
+    // this.$allSaySomething.forEach(element => {
+    //   element.setAttribute('text', this.$input.value)
+    // });
   }
 }
 
-window.customElements.define('my-app', App);
+window.customElements.define('maltem-card', Card);
