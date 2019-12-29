0637795a159a09a0e31bc8421412fbf7c39139ea
unknown <rajimohan.gopalan@logicline.de>
2019-12-29 20:25:08 +0800

    Feature: drag and drop added

--- src/components/cardItems.js
+++ src/components/cardItems.js
@@ -1,45 +1,5 @@
-const template = document.createElement('template');
-
-template.innerHTML = `
-<style>
-.list-items {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-content: start;
-  padding: 0 0.6rem 0.5rem;
-  overflow-y: auto;
-  height : 200px;
-}
-.list-items::-webkit-scrollbar {
-  width: 1.6rem;
-}
-.list-items::-webkit-scrollbar-thumb {
-  background-color: #c4c9cc;
-  border-right: 0.6rem solid #e2e4e6;
-}
-.list-items li {
-  font-size: 1.4rem;
-  font-weight: 400;
-  line-height: 1.3;
-  background-color: #fff;
-  padding: 0.65rem 0.6rem;
-  color: #4d4d4d;
-  border-bottom: 0.1rem solid #ccc;
-  border-radius: 0.3rem;
-  margin-bottom: 0.6rem;
-  word-wrap: break-word;
-  cursor: pointer;
-}
-.list-items li:last-of-type {
-  margin-bottom: 0;
-}
-.list-items li:hover {
-  background-color: #eee;
-}
-  </style>
-  <ul class="list-items">
-</ul>`;
+let start = 0;
+let target = undefined;
 
 class CardItems extends HTMLElement {
   constructor() {
@@ -50,8 +10,8 @@ class CardItems extends HTMLElement {
 
   handleEvent(e) {
     this.$listItem.forEach((item) => {
-      if(item.id == e.target.id) {
-        item.textContent = this.cards.filter(item => item.id ==  e.target.id)[0].description;
+      if (item.id == e.target.id) {
+        item.textContent = this.cards.filter(item => item.id == e.target.id)[0].description;
       }
     });
   }
@@ -66,22 +26,102 @@ class CardItems extends HTMLElement {
     this._render();
   };
 
+  async postCards(card) {
+    const cards = await fetch('http://localhost:3000/cards', {
+      method: 'post', body: JSON.stringify(card), headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    window.location.reload(false);
+  };
+
+  dragstart(e) {
+    start = e.target.attributes.column.value;
+    target = e.target;
+  }
+
+
+  dragover(e) {
+    e.preventDefault();
+  }
+
+  dragDrop(e) {
+    //get Drop details.
+    if (e.target.attributes.column.value !== start) {
+      this.postCards({
+        "title": target.innerHTML,
+        "description": target.attributes.desc.value,
+        "columnId": parseInt(this.id)
+      });
+    }
+  }
+
+
   _render() {
+
+    const cardtemplate = document.createElement('template');
+
+    cardtemplate.innerHTML = `
+                            <style>
+                            .list-items {
+                              flex: 1;
+                              display: flex;
+                              flex-direction: column;
+                              align-content: start;
+                              padding: 0 0.6rem 0.5rem;
+                              overflow-y: auto;
+                              height : 200px;
+                            }
+                            .list-items::-webkit-scrollbar {
+                              width: 1.6rem;
+                            }
+                            .list-items::-webkit-scrollbar-thumb {
+                              background-color: #c4c9cc;
+                              border-right: 0.6rem solid #e2e4e6;
+                            }
+                            .list-items li {
+                              font-size: 1.4rem;
+                              font-weight: 400;
+                              line-height: 1.3;
+                              background-color: #fff;
+                              padding: 0.65rem 0.6rem;
+                              color: #4d4d4d;
+                              border-bottom: 0.1rem solid #ccc;
+                              border-radius: 0.3rem;
+                              margin-bottom: 0.6rem;
+                              word-wrap: break-word;
+                              cursor: pointer;
+                            }
+                            .list-items li:last-of-type {
+                              margin-bottom: 0;
+                            }
+                            .list-items li:hover {
+                              background-color: #eee;
+                            }
+                              </style>
+                              <ul class="list-items">
+                            </ul>`;
+
     this.id = this.getAttribute('id');
-      for(let i=0;i<this.cards.length;i++) {
-      if(this.cards[i].columnId === parseInt(this.id)) {
-        template.content.querySelector('.list-items').innerHTML += `
-        <li id="${this.cards[i].id}">${this.cards[i].title}</li>    
+    for (let i = 0; i < this.cards.length; i++) {
+      if (this.cards[i].columnId === parseInt(this.id)) {
+        cardtemplate.content.querySelector('.list-items').innerHTML += `
+        <li id="${this.cards[i].id}" desc="${this.cards[i].description}" column="${this.cards[i].columnId}" draggable="true">${this.cards[i].title}</li>    
         `;
       }
     }
-    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this._shadowRoot.appendChild(cardtemplate.content.cloneNode(true));
     this.$listItem = this._shadowRoot.querySelectorAll('li');
     this.$listItem.forEach((item) => {
-      item.addEventListener('click',this);
+      item.addEventListener('click', this);
+      item.addEventListener('dragstart', this.dragstart);
+      item.addEventListener('dragend', this.dragover);
+      item.addEventListener('dragover', this.dragover);
+      item.addEventListener('drop', this.dragDrop.bind(this));
     });
     this.$span = this._shadowRoot.querySelector('span');
   };
+
 
 }
 
