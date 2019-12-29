e553a65eb7b649d62bf69164772fb92998dbf5ba
unknown <rajimohan.gopalan@logicline.de>
2019-12-29 12:18:17 +0800

    code cleaning : remove unwanted styles

--- src/index.js
+++ src/index.js
@@ -4,16 +4,36 @@ const template = document.createElement('template');
 
 template.innerHTML = `
 <style>
-.list {
-  flex: 0 0 27rem;
-  display: flex;
-  flex-direction: column;
-  background-color: #e2e4e6;
-  max-height: calc(100vh - 11.8rem);
-  border-radius: 0.3rem;
-  margin-right: 1rem;
-  margin: 20px;
+/* Lists */
+
+.lists-container::-webkit-scrollbar {
+    height: 2.4rem;
 }
+
+.lists-container::-webkit-scrollbar-thumb {
+    background-color: #66a3c7;
+    border: 0.8rem solid #0079bf;
+    border-top-width: 0;
+}
+
+.lists-container {
+    display: flex;
+    align-items: start;
+    padding: 0 0.8rem 0.8rem;
+    overflow-x: auto;
+    /* height: calc(100vh - 8.6rem); */
+}
+
+.list {
+    flex: 0 0 27rem;
+    display: flex;
+    flex-direction: column;
+    background-color: #e2e4e6;
+    max-height: calc(100vh - 11.8rem);
+    border-radius: 0.3rem;
+    margin-right: 1rem;
+}
+
 
 .list:last-of-type {
   margin-right: 0;
